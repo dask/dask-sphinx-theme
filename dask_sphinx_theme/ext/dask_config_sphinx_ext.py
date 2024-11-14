813d4c11f7e7a9a852b846ec9e6cfc7e8d58444e
James Bourbeau <jrbourbeau@users.noreply.github.com>
2024-11-14 14:09:30 -0600

    Add fix for downloading config files from GitHub (#91)

--- dask_sphinx_theme/ext/dask_config_sphinx_ext.py
+++ dask_sphinx_theme/ext/dask_config_sphinx_ext.py
@@ -1,3 +1,4 @@
+import dask_sphinx_theme
 import requests
 import yaml
 from docutils import nodes
@@ -5,7 +6,11 @@ from docutils.parsers.rst import Directive, directives
 
 
 def get_remote_yaml(url):
-    r = requests.get(url)
+    r = requests.get(
+        url,
+        headers={"User-Agent": f"Dask sphinx theme {dask_sphinx_theme.__version__}"},
+    )
+    r.raise_for_status()
     return yaml.safe_load(r.text)
 
 
